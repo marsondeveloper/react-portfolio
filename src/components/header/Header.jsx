55b38f9de70460c9b31cc578a505a9fc564fe0c0
marsondeveloper <marson4ik2019@gmail.com>
2026-01-28 10:47:04 +0200

    commit three

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,28 @@
+import { useState } from 'react';
 import Nav from '../nav/Nav'
 import './Header.css'
 import { FaXing } from "react-icons/fa";
+import { FaBarsStaggered } from "react-icons/fa6";
+
+
 
 const Header = () => {
+    const [isMenuOpen, setisMenuOpen] = useState(false)
+
+    const toogleMenu = () => {
+        setisMenuOpen(!isMenuOpen)
+    }
+
   return (
       <header className="header">
           <div className="container">
               <div className="header-wrapper">
                  <a href="" className="logo">Marlen</a>
                       <Nav/>
-                  {/* <div className="nav-menu-btn">
-                      <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M3 4H21V6H3V4ZM9 11H21V13H9V11ZM3 18H21V20H3V18Z"></path></svg>
-                  </div> */}
+                  <div className="nav-menu-btn" onClick={toogleMenu}>
+                      {isMenuOpen ? <FaXing/> : <FaBarsStaggered/>}
+                      {/* <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M3 4H21V6H3V4ZM9 11H21V13H9V11ZM3 18H21V20H3V18Z"></path></svg> */}
+                  </div>
               </div>
           </div>
       </header>
